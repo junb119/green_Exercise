5dcdba21bf320c1469d88ec63810176d2aee1a66
junb119 <emfowkd@gmail.com>
2024-03-20 14:41:09 +0900

    2024-03-20 filter mixitup2

--- jquery/filter_ex (2)/script_mixitup.js
+++ jquery/filter_ex (2)/script_mixitup.js
@@ -1,7 +1,18 @@
 var mixer = mixitup('.product_list', {
   selectors: {
   target: '.item'
+  },
+  "animation" : {
+    "duration": 1500,
+    "nudge": true,
+    "reverseOut": false,
+    "effects": "fade translateX(20%) translateZ(-100px) rotateX(90deg)"
   }
 });
 
+$('#filter').change(function () {
+  let target = $(this).val()
+  mixer.filter(target)
+
+})
 
